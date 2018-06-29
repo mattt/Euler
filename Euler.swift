6ad479a5fe050a9767b1698ddb389614ff8bfb47
Mattt <mattt@me.com>
2018-06-29 06:33:12 -0700

    Update to Swift 4.2

--- Euler.swift
+++ Euler.swift
@@ -1,6 +1,6 @@
 // Euler.swift
 //
-// Copyright (c) 2014–2015 Mattt Thompson (http://mattt.me)
+// Copyright (c) 2014–2018 Mattt (https://mat.tt)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -29,11 +29,11 @@ import Foundation
 
 // MARK: Pi
 
-let π = M_PI
+let π = Double.pi
 
 // MARK: Tau
 
-let 𝝉 = M_PI * 2
+let 𝝉 = Double.pi * 2
 
 // MARK: e
 
@@ -43,80 +43,80 @@ let 𝑒 = M_E
 
 // MARK: Negation
 
-prefix operator ¬ {}
+prefix operator ¬
 prefix func ¬ (value: Bool) -> Bool {
     return !value
 }
 
-prefix operator ~ {}
+prefix operator ~
 prefix func ~ (value: Bool) -> Bool {
     return !value
 }
 
 // MARK: Logical Conjunction
 
-infix operator ∧ { associativity left precedence 120 }
-func ∧ (left: Bool, @autoclosure right:  () -> Bool) -> Bool {
+infix operator ∧ : LogicalConjunctionPrecedence
+func ∧ (left: Bool, right: @autoclosure () -> Bool) -> Bool {
     return left && right()
 }
 
 // MARK: Logical Disjunction
 
-infix operator ∨ { associativity left precedence 110 }
-func ∨ (left: Bool, @autoclosure right:  () -> Bool) -> Bool {
+infix operator ∨ : LogicalDisjunctionPrecedence
+func ∨ (left: Bool, right: @autoclosure () -> Bool) -> Bool {
     return left || right()
 }
 
 // MARK: Logical XOR
 
-infix operator ⊻ { associativity left precedence 140 }
-func ⊻ (left: Bool, @autoclosure right:  () -> Bool) -> Bool {
+infix operator ⊻ : LogicalDisjunctionPrecedence
+func ⊻ (left: Bool, right: @autoclosure () -> Bool) -> Bool {
     return left != right()
 }
 
-infix operator ⊕ { associativity left precedence 140 }
-func ⊕ (left: Bool, @autoclosure right:  () -> Bool) -> Bool {
+infix operator ⊕ : LogicalDisjunctionPrecedence
+func ⊕ (left: Bool, right: @autoclosure () -> Bool) -> Bool {
     return left != right()
 }
 
-infix operator ↮ { associativity left precedence 140 }
-func ↮ (left: Bool, @autoclosure right:  () -> Bool) -> Bool {
+infix operator ↮ : LogicalDisjunctionPrecedence
+func ↮ (left: Bool, right: @autoclosure () -> Bool) -> Bool {
     return left != right()
 }
 
-infix operator ≢ { associativity left precedence 140 }
-func ≢ (left: Bool, @autoclosure right:  () -> Bool) -> Bool {
+infix operator ≢ : LogicalDisjunctionPrecedence
+func ≢ (left: Bool, right: @autoclosure () -> Bool) -> Bool {
     return left != right()
 }
 
 // MARK: Logical NAND
 
-infix operator ⊼ { associativity left precedence 120 }
-func ⊼ (left: Bool, @autoclosure right:  () -> Bool) -> Bool {
+infix operator ⊼ : LogicalConjunctionPrecedence
+func ⊼ (left: Bool, right: @autoclosure () -> Bool) -> Bool {
     return ¬(left ∧ right())
 }
 
-infix operator ↑ { associativity left precedence 120 }
-func ↑ (left: Bool, @autoclosure right:  () -> Bool) -> Bool {
+infix operator ↑ : LogicalConjunctionPrecedence
+func ↑ (left: Bool, right: @autoclosure () -> Bool) -> Bool {
     return ¬(left ∧ right())
 }
 
 // MARK: Logical NOR
 
-infix operator ⊽ { associativity left precedence 110 }
-func ⊽ (left: Bool, @autoclosure right:  () -> Bool) -> Bool {
+infix operator ⊽ : LogicalDisjunctionPrecedence
+func ⊽ (left: Bool, right: @autoclosure () -> Bool) -> Bool {
     return ¬(left ∨ right())
 }
 
-infix operator ↓ { associativity left precedence 110 }
-func ↓ (left: Bool, @autoclosure right:  () -> Bool) -> Bool {
+infix operator ↓ : LogicalDisjunctionPrecedence
+func ↓ (left: Bool, right: @autoclosure () -> Bool) -> Bool {
     return ¬(left ∨ right())
 }
 
 // MARK: Logical Assertion
 
-prefix operator ⊦ {}
-prefix func ⊦ (@autoclosure condition:  () -> Bool) {
+prefix operator ⊦
+prefix func ⊦ (condition: @autoclosure () -> Bool) {
     assert(condition(), "Assertion Failed")
 }
 
@@ -124,78 +124,78 @@ prefix func ⊦ (@autoclosure condition:  () -> Bool) {
 
 // MARK: Multiplication
 
-infix operator × { associativity left precedence 150 }
+infix operator × : MultiplicationPrecedence
 func × (left: Double, right: Double) -> Double {
     return left * right
 }
 
 // MARK: Division
 
-infix operator ÷ { associativity left precedence 150 }
+infix operator ÷ : MultiplicationPrecedence
 func ÷ (left: Double, right: Double) -> Double {
     return left / right
 }
 
-infix operator ∕ { associativity left precedence 150 }
+infix operator ∕ : MultiplicationPrecedence
 func ∕ (left: Double, right: Double) -> Double {
     return left / right
 }
 
 // MARK: Square Root
 
-prefix operator √ {}
+prefix operator √
 prefix func √ (number: Double) -> Double {
     return sqrt(number)
 }
 
 // MARK: Cube Root
 
-prefix operator ∛ {}
+prefix operator ∛
 prefix func ∛ (number: Double) -> Double {
     return cbrt(number)
 }
 
 // MARK: Tesseract Root
 
-prefix operator ∜ {}
+prefix operator ∜
 prefix func ∜ (number: Double) -> Double {
     return pow(number, 1.0 / 4.0)
 }
 
 // MARK: Plus / Minus
 
-infix operator ± { associativity left precedence 140 }
+infix operator ± : AdditionPrecedence
 func ± (left: Double, right: Double) -> (Double, Double) {
     return (left + right, left - right)
 }
 
-prefix operator ± {}
+prefix operator ±
 prefix func ± (value: Double) -> (Double, Double) {
     return 0 ± value
 }
 
 // MARK: Minus / Plus
 
-infix operator ∓ { associativity left precedence 140 }
+infix operator ∓ : AdditionPrecedence
 func ∓ (left: Double, right: Double) -> (Double, Double) {
     return (left - right, left + right)
 }
 
-prefix operator ∓ {}
+prefix operator ∓
 prefix func ∓ (value: Double) -> (Double, Double) {
     return 0 ∓ value
 }
 
 // MARK: Divides
 
-infix operator ∣ { associativity left precedence 150 }
+infix operator ∣ : MultiplicationPrecedence
 func ∣ (left: Int, right: Int) -> Bool {
     return left % right == 0
 }
 
 // MARK: Does Not Divide
 
-infix operator ∤ { associativity left }
+infix operator ∤ : MultiplicationPrecedence
 func ∤ (left: Int, right: Int) -> Bool {
     return ¬(left ∣ right)
 }
@@ -204,9 +204,9 @@ func ∤ (left: Int, right: Int) -> Bool {
 
 // MARK: Set Membership
 
-infix operator ∈ { associativity left }
+infix operator ∈ : ComparisonPrecedence
 func ∈<T: Equatable> (left: T, right: [T]) -> Bool {
-    return contains(right, left)
+    return right.contains(left)
 }
 
 func ∈<T> (left: T, right: Set<T>) -> Bool {
@@ -215,7 +215,7 @@ func ∈<T> (left: T, right: Set<T>) -> Bool {
 
 // MARK: Set Non-Membership
 
-infix operator ∉ { associativity left }
+infix operator ∉ : ComparisonPrecedence
 func ∉<T: Equatable> (left: T, right: [T]) -> Bool {
     return ¬(left ∈ right)
 }
@@ -226,7 +226,7 @@ func ∉<T> (left: T, right: Set<T>) -> Bool {
 
 // MARK: Converse Set Membership
 
-infix operator ∋ { associativity left }
+infix operator ∋ : ComparisonPrecedence
 func ∋<T: Equatable> (left: [T], right: T) -> Bool {
     return right ∈ left
 }
@@ -237,7 +237,7 @@ func ∋<T> (left: Set<T>, right: T) -> Bool {
 
 // MARK: Converse Set Non-Membership
 
-infix operator ∌ { associativity left }
+infix operator ∌ : ComparisonPrecedence
 func ∌<T: Equatable> (left: [T], right: T) -> Bool {
     return right ∉ left
 }
@@ -248,7 +248,7 @@ func ∌<T> (left: Set<T>, right: T) -> Bool {
 
 // MARK: Set Intersection
 
-infix operator ∩ { associativity left }
+infix operator ∩ : ComparisonPrecedence
 func ∩<T: Equatable> (left: [T], right: [T]) -> [T] {
     var intersection: [T] = []
     for value in left {
@@ -256,17 +256,17 @@ func ∩<T: Equatable> (left: [T], right: [T]) -> [T] {
             intersection.append(value)
         }
     }
-
+    
     return intersection
 }
 
 func ∩<T> (left: Set<T>, right: Set<T>) -> Set<T> {
-    return left.intersect(right)
+    return left.intersection(right)
 }
 
 // MARK: Set Union
 
-infix operator ∪ { associativity left }
+infix operator ∪ : ComparisonPrecedence
 func ∪<T: Equatable> (left: [T], right: [T]) -> [T] {
     var union: [T] = []
     for value in left + right {
@@ -274,7 +274,7 @@ func ∪<T: Equatable> (left: [T], right: [T]) -> [T] {
             union.append(value)
         }
     }
-
+    
     return union
 }
 
@@ -284,35 +284,35 @@ func ∪<T> (left: Set<T>, right: Set<T>) -> Set<T> {
 
 // MARK: Subset
 
-infix operator ⊆ { associativity left }
+infix operator ⊆ : ComparisonPrecedence
 func ⊆<T: Equatable> (left: [T], right: [T]) -> Bool {
     return left == right || (left ⊂ right)
 }
 
 func ⊆<T> (left: Set<T>, right: Set<T>) -> Bool {
-    return left.isSubsetOf(right)
+    return left.isSubset(of: right)
 }
 
 // MARK: Proper Subset
 
-infix operator ⊂ { associativity left }
+infix operator ⊂ : ComparisonPrecedence
 func ⊂<T: Equatable> (left: [T], right: [T]) -> Bool {
     for value in left {
         if ¬(value ∈ right) {
             return false
         }
     }
-
+    
     return true
 }
 
 func ⊂<T> (left: Set<T>, right: Set<T>) -> Bool {
-    return left.isStrictSubsetOf(right)
+    return left.isStrictSubset(of: right)
 }
 
 // MARK: Not A Subset Of
 
-infix operator ⊄ { associativity left }
+infix operator ⊄ : ComparisonPrecedence
 func ⊄<T: Equatable> (left: [T], right: [T]) -> Bool {
     return ¬(left ⊂ right)
 }
@@ -323,7 +323,7 @@ func ⊄<T> (left: Set<T>, right: Set<T>) -> Bool {
 
 // MARK: Superset
 
-infix operator ⊇ { associativity left }
+infix operator ⊇ : ComparisonPrecedence
 func ⊇<T: Equatable> (left: [T], right: [T]) -> Bool {
     return right ⊆ left
 }
@@ -334,7 +334,7 @@ func ⊇<T> (left: Set<T>, right: Set<T>) -> Bool {
 
 // MARK: Proper Superset
 
-infix operator ⊃ { associativity left }
+infix operator ⊃ : ComparisonPrecedence
 func ⊃<T: Equatable> (left: [T], right: [T]) -> Bool {
     return right ⊂ left
 }
@@ -345,7 +345,7 @@ func ⊃<T> (left: Set<T>, right: Set<T>) -> Bool {
 
 // MARK: Not A Superset Of
 
-infix operator ⊅ { associativity left }
+infix operator ⊅ : ComparisonPrecedence
 func ⊅<T: Equatable> (left: [T], right: [T]) -> Bool {
     return ¬(left ⊃ right)
 }
@@ -358,32 +358,32 @@ func ⊅<T> (left: Set<T>, right: Set<T>) -> Bool {
 
 // MARK: Summation
 
-prefix operator ∑ {}
+prefix operator ∑
 prefix func ∑ (values: [Double]) -> Double {
-    return reduce(values, 0.0, +)
+    return values.reduce(0.0, +)
 }
 
 // MARK: Cartesian Product
 
-prefix operator ∏ {}
+prefix operator ∏
 prefix func ∏ (values: [Double]) -> Double {
-    return reduce(values, 1.0, *)
+    return values.reduce(1.0, *)
 }
 
 // MARK: - Vectors -
 
 // MARK: Dot Product
 
-infix operator ⋅ {}
+infix operator ⋅
 func ⋅ (left: [Double], right: [Double]) -> Double {
     precondition(left.count == right.count, "arguments must have same count")
-
+    
     var product: [Double] = []
-    for (index, _) in enumerate(left) {
+    for (index, _) in left.enumerated() {
         let (a, b) = (left[index], right[index])
         product.append(a * b)
     }
-
+    
     return ∑product
 }
 
@@ -393,20 +393,20 @@ func × (left: (Double, Double, Double), right: (Double, Double, Double)) -> (Do
     let a = left.1 * right.2 - left.2 * right.1
     let b = left.2 * right.0 - left.0 * right.2
     let c = left.0 * right.1 - left.1 * right.0
-
+    
     return (a, b, c)
 }
 
 // Mark: Norm
 
-prefix operator ‖ {}
+prefix operator ‖
 prefix func ‖ (vector: [Double]) -> Double {
     return √(∑vector.map({$0 * $0}))
 }
 
 // MARK: Angle
 
-infix operator ⦡ {}
+infix operator ⦡
 func ⦡ (left: [Double], right: [Double]) -> Double {
     return acos((left ⋅ right) / (‖left * ‖right))
 }
@@ -415,56 +415,56 @@ func ⦡ (left: [Double], right: [Double]) -> Double {
 
 // MARK: Equality
 
-infix operator ⩵ { associativity left }
+infix operator ⩵ : ComparisonPrecedence
 func ⩵<T: Equatable> (left: T, right: T) -> Bool {
     return left == right
 }
 
 // MARK: Inequality
 
-infix operator ≠ { associativity left }
+infix operator ≠ : ComparisonPrecedence
 func ≠<T: Equatable> (left: T, right: T) -> Bool {
     return left != right
 }
 
 // MARK: Less Than Or Equal To
 
-infix operator ≤ { associativity left }
+infix operator ≤ : ComparisonPrecedence
 func ≤<T: Comparable> (left: T, right: T) -> Bool {
     return left <= right
 }
 
 // MARK: Less Than And Not Equal To
 
-infix operator ≨ { associativity left }
+infix operator ≨ : ComparisonPrecedence
 func ≨<T: Comparable> (left: T, right: T) -> Bool {
     return left < right && left != right
 }
 
 // MARK: Greater Than Or Equal To
 
-infix operator ≥ { associativity left }
+infix operator ≥ : ComparisonPrecedence
 func ≥<T: Comparable> (left: T, right: T) -> Bool {
     return left >= right
 }
 
 // MARK: Greater Than And Not Equal To
 
-infix operator ≩ { associativity left }
+infix operator ≩ : ComparisonPrecedence
 func ≩<T: Comparable> (left: T, right: T) -> Bool {
     return left > right && left != right
 }
 
 // MARK: Between
 
-infix operator ≬ { associativity left }
+infix operator ≬ : ComparisonPrecedence
 func ≬<T: Comparable> (left: T, right: (T, T)) -> Bool {
     return left > right.0 && left < right.1
 }
 
 // MARK: Approximate Equality
 
-infix operator ≈ { associativity left }
+infix operator ≈ : ComparisonPrecedence
 func ≈(left: Double, right: Double) -> Bool {
     let 𝜺 = 1e-3
     return abs(nextafter(left, right) - right) < 𝜺
@@ -472,7 +472,7 @@ func ≈(left: Double, right: Double) -> Bool {
 
 // MARK: Approximate Inequality
 
-infix operator ≉ { associativity left }
+infix operator ≉ : ComparisonPrecedence
 func ≉(left: Double, right: Double) -> Bool {
     return !(left ≈ right)
 }
@@ -482,8 +482,8 @@ func ≉(left: Double, right: Double) -> Bool {
 
 // MARK: 1st Derivative
 
-postfix operator ′ {}
-postfix func ′(function: (Double) -> (Double)) -> (Double) -> (Double) {
+postfix operator ′
+postfix func ′(function: @escaping (Double) -> (Double)) -> (Double) -> (Double) {
     let h = 1e-3
     return { (x) in
         return round((function(x + h) - function(x - h)) / (2 * h) / h) * h
@@ -492,44 +492,44 @@ postfix func ′(function: (Double) -> (Double)) -> (Double) -> (Double) {
 
 // MARK: 2nd Derivative
 
-postfix operator ′′ {}
-postfix func ′′(function: (Double) -> (Double)) -> (Double) -> (Double) {
+postfix operator ′′
+postfix func ′′(function: @escaping (Double) -> (Double)) -> (Double) -> (Double) {
     return (function′)′
 }
 
 // MARK: 3rd Derivative
 
-postfix operator ′′′ {}
-postfix func ′′′(function: (Double) -> (Double)) -> (Double) -> (Double) {
+postfix operator ′′′
+postfix func ′′′(function: @escaping (Double) -> (Double)) -> (Double) -> (Double) {
     return ((function′)′)′
 }
 
 // MARK: Nth Derivative
 
-infix operator ′ { associativity left }
-func ′(var left: (Double -> Double), right: UInt) -> (Double) -> (Double) {
-    return reduce(0..<right, left) { (function, _) in
+infix operator ′ : MultiplicationPrecedence
+func ′(left: inout (Double) -> Double, right: UInt) -> (Double) -> (Double) {
+    return (0..<right).reduce(left) { (function, _) in
         return function′
     }
 }
 
 // MARK: Definite Integral
 
-infix operator ∫ { associativity left }
+infix operator ∫ : MultiplicationPrecedence
 func ∫(left: (a: Double, b: Double), right: (Double) -> (Double)) -> Double {
     let n = Int(1e2 + 1)
     let h = (left.b - left.a) / Double(n)
-
-    return (h / 3.0) * (reduce(1..<n, right(left.a)) {
+    
+    return (h / 3.0) * (1..<n).reduce(right(left.a)) {
         let coefficient = $1 % 2 == 0 ? 4.0 : 2.0
         return $0 + coefficient * right(left.a + Double($1) * h)
-    } + right(left.b))
+        } + right(left.b)
 }
 
 // MARK: Indefinite Integral / Antiderivative
 
-prefix operator ∫ {}
-prefix func ∫(function: (Double) -> (Double)) -> (Double) -> (Double) {
+prefix operator ∫
+prefix func ∫(function: @escaping (Double) -> (Double)) -> (Double) -> (Double) {
     return { x in
         return (0, x)∫function
     }
@@ -539,8 +539,8 @@ prefix func ∫(function: (Double) -> (Double)) -> (Double) -> (Double) {
 
 // MARK: Composition
 
-infix operator ∘ { associativity left }
-func ∘<T>(left: (T) -> (T), right: (T) -> (T)) -> (T) -> (T) {
+infix operator ∘ : MultiplicationPrecedence
+func ∘<T>(left: @escaping (T) -> (T), right: @escaping (T) -> (T)) -> (T) -> (T) {
     return { (x) in
         left(right(x))
     }
