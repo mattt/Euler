d8d08390dcb4bed02c993a6a27efe6a1ce879752
Mattt <mattt@me.com>
2018-06-29 07:25:37 -0700

    Add example code to playground

--- Euler.playground/Contents.swift
+++ Euler.playground/Contents.swift
@@ -1,1 +1,46 @@
+import Foundation
 import Euler
+
+// Constants
+
+𝑒
+
+// Logic
+
+¬true
+
+// Arithmetic
+
+3 × 4
+
+// Sets
+
+let prime: Set<Int> = [2, 3, 5, 7, 11]
+let fibonacci: Set<Int> = [1, 1, 2, 3, 5, 8, 13]
+prime ∩ fibonacci
+
+// Sequences
+
+∑[1, 2, 3, 4, 5]
+
+// Vectors
+
+[1, 2] ⋅ [3, 4]
+
+// Comparison
+
+7 ≠ 9
+
+// Calculus
+
+let f: (Double) -> Double = sin
+let fPrime = f′
+
+f(π)
+fPrime(π)
+
+// Functions
+
+let g: (Double) -> Double = cos
+
+(f ∘ g)(π)
