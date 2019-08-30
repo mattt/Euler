1fd0375fd609ccc84a7eb1d881cfbef8d3d35c16
Mattt <mattt@me.com>
2019-08-30 08:14:32 -0700

    Enable logical XOR and logical NAND test

--- Tests/EulerTests/LogicTests.swift
+++ Tests/EulerTests/LogicTests.swift
@@ -6,38 +6,38 @@ final class LogicTests: XCTestCase {
         XCTAssertEqual(¬true, !true)
         XCTAssertEqual(~true, !true)
     }
-    
+
     func testLogicalConjunction() {
         XCTAssertEqual(true ∧ false, true && false)
     }
-    
+
     func testLogicalDisjunction() {
         XCTAssertEqual(true ∨ false, true || false)
     }
-    
-//    func testLogicalXOR() {
-//        XCTAssertEqual(true ⊻ false, true ^ false)
-//        XCTAssertEqual(true ⊕ false, true ^ false)
-//        XCTAssertEqual(true ↮ false, true ^ false)
-//        XCTAssertEqual(true ≢ false, true ^ false)
-//    }
-    
-//    func testLogicalNAND() {
-//        XCTAssertEqual(true ⊼ false, !(true ^ false))
-//        XCTAssertEqual(true ↑ false, !(true ^ false))
-//    }
-    
+
+    func testLogicalXOR() {
+        XCTAssertEqual(true ⊻ false, true != false)
+        XCTAssertEqual(true ⊕ false, true != false)
+        XCTAssertEqual(true ↮ false, true != false)
+        XCTAssertEqual(true ≢ false, true != false)
+    }
+
+    func testLogicalNAND() {
+        XCTAssertEqual(true ⊼ false, !(true && false))
+        XCTAssertEqual(true ↑ false, !(true && false))
+    }
+
     func testLogicalNOR() {
         XCTAssertEqual(true ⊽ false, !(true || false))
         XCTAssertEqual(true ↓ false, !(true || false))
     }
-    
+
     static var allTests = [
         ("testLogicalNegation", testLogicalNegation),
         ("testLogicalConjunction", testLogicalConjunction),
         ("testLogicalDisjunction", testLogicalDisjunction),
-//        ("testLogicalXOR", testLogicalXOR),
-//        ("testLogicalNAND", testLogicalNAND),
+        ("testLogicalXOR", testLogicalXOR),
+        ("testLogicalNAND", testLogicalNAND),
         ("testLogicalNOR", testLogicalNOR)
     ]
 }
