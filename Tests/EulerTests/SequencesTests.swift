3e38ecbeb3124b3de56be77a69e84774f23ae651
Mattt <mattt@me.com>
2019-08-30 11:16:34 -0700

    Add SequenceTests.swift

---
+++ Tests/EulerTests/SequencesTests.swift
@@ -0,0 +1,14 @@
+import XCTest
+@testable import Euler
+
+final class SequencesTests: XCTestCase {
+    func testSummation() {
+        XCTAssertEqual(∑([] as [Int]), 0)
+        XCTAssertEqual(∑([1, 2, 3, 4, 5] as [Int]), 15)
+    }
+
+    func testCartesianProduct() {
+        XCTAssertEqual(∏([] as [Int]), 1)
+        XCTAssertEqual(∏([1, 2, 3, 4, 5] as [Int]), 120)
+    }
+}
