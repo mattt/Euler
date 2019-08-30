201c6f0a8a89ba97f225c2209ac2c65c189af1b8
Mattt <mattt@me.com>
2019-08-30 11:16:59 -0700

    Redefine Summation and Cartesian Product operators in terms of Sequence type

--- Sources/Euler/Sequences.swift
+++ Sources/Euler/Sequences.swift
@@ -1,13 +1,13 @@
 // MARK: Summation
 
 prefix operator ∑
-public prefix func ∑ <T>(values: [T]) -> T where T : Numeric & ExpressibleByIntegerLiteral {
+public prefix func ∑ <S: Sequence>(values: S) -> S.Element where S.Element : Numeric & ExpressibleByIntegerLiteral {
     return values.reduce(0, +)
 }
 
 // MARK: Cartesian Product
 
 prefix operator ∏
-public prefix func ∏ <T>(values: [T]) -> T where T : Numeric & ExpressibleByIntegerLiteral {
+public prefix func ∏ <S: Sequence>(values: S) -> S.Element where S.Element : Numeric & ExpressibleByIntegerLiteral {
     return values.reduce(1, *)
 }
