da37f6cc23d9d0ba6367c01863df12d573df7fc7
Mattt <mattt@me.com>
2021-04-20 09:52:58 -0700

    Add documentation to functional operators

--- Sources/Euler/Functions.swift
+++ Sources/Euler/Functions.swift
@@ -1,6 +1,13 @@
 // MARK: Composition
 
+// The functional composition operator.
 infix operator ∘ : MultiplicationPrecedence
+
+/// Returns the composition of two functions.
+///
+/// - Parameters:
+///   - lhs: A function.
+///   - rhs: Another function.
 public func ∘ <T>(lhs: @escaping (T) -> (T), rhs: @escaping (T) -> (T)) -> (T) -> (T) {
     return { (x) in
         lhs(rhs(x))
