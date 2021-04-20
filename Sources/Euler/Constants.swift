9396f4de3a40d6038bf540bb6b9a42161f582e32
Mattt <mattt@me.com>
2021-04-20 09:52:41 -0700

    Add documentation to constants

--- Sources/Euler/Constants.swift
+++ Sources/Euler/Constants.swift
@@ -1,17 +1,13 @@
 import Foundation
 
-// MARK: Pi
-
+/// Pi (3.1415926535897931)
 public let π = Double.pi
 
-// MARK: Tau
-
+/// Tau (6.2831853071795862)
 public let 𝝉 = Double.pi * 2
 
-// MARK: e
-
+/// e (2.7182818284590451)
 public let 𝑒 = M_E
 
-// MARK: Machine Epsilon
-
+/// Machine epsilon (2.2204460492503131E-16)
 public let ε = Double.ulpOfOne
