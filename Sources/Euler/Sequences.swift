7bef9799dd0932a32211faa1d8b4e51a4d68992f
Mattt <mattt@me.com>
2021-04-20 09:53:18 -0700

    Add documentation to sequence operators

--- Sources/Euler/Sequences.swift
+++ Sources/Euler/Sequences.swift
@@ -1,13 +1,25 @@
 // MARK: Summation
 
+/// The summation operator.
 prefix operator ∑
+
+/// Returns the summation of a sequence of numbers.
+///
+/// - Parameters:
+///   - values: A sequence of numbers.
 public prefix func ∑ <S: Sequence>(values: S) -> S.Element where S.Element : Numeric & ExpressibleByIntegerLiteral {
     return values.reduce(0, +)
 }
 
 // MARK: Cartesian Product
 
+/// The Cartesian product operator.
 prefix operator ∏
+
+/// Returns the Cartesian product of a sequence of numbers.
+///
+/// - Parameters:
+///   - values: A sequence of numbers.
 public prefix func ∏ <S: Sequence>(values: S) -> S.Element where S.Element : Numeric & ExpressibleByIntegerLiteral {
     return values.reduce(1, *)
 }
